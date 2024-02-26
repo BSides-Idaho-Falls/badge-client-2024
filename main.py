1f29f56b5fb8a4dc01c54122a2a8cffc598d964e
meecles <meecles@meecles.net>
2024-02-25 17:23:41 -0800

    update main

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import uasyncio as asyncio
-import time
 from machine import WDT
 
 from library import badge, atomics
 
 if __name__ == '__main__':
     atomics.wdt = WDT(timeout=8388)
-    while True:
-        asyncio.run(badge.start_main())
-        print("Restarting main thread...")
-        time.sleep(1)
+    asyncio.run(badge.start_main())
